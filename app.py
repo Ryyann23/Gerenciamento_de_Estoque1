52329a68af5d09128c9cdf1e7ea52775e5132ee4
Ryyann23 <ryanbarroso2303@gmail.com>
2025-06-07 12:35:13 -0300

    .

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pytz
 import os
 
 app = Flask(__name__)
-app.secret_key = ''
+app.secret_key = 'teste123'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "database.db")}'
